ba93750dd1e59550eba671dec29e7b749c742dd0
hai <hai@LAPTOP-4DV9N6FJ>
2021-08-01 19:24:09 +0530

    Fifth commit
    Upload all files

---
+++ CONVERT/src/a/TEST8.java
@@ -0,0 +1,15 @@
+package a;
+
+public class TEST8 {
+
+	public static void main(String[] args) {
+byte b;
+int i = 1;
+double d = 1.123;
+i = (int) d;
+System.out.println("d: "+ d +" i:"  +i);
+b= (byte) d;
+System.out.println("d: "+ d +" b:"  +b);
+	}
+
+}
---
+++ CONVERT/src/module-info.java
@@ -0,0 +1,2 @@
+module CONVERT {
+}
